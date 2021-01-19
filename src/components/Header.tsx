ab95c8ad0f7149cc70eef8e15beb0341fdce3174
unknown <joticd@gmail.com>
2021-01-20 00:07:51 +0100

    fix: jsx tags for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,14 @@ import Search from './Search';
 
 const Header:React.FC = () => {
     return (
-        <div>
-            <Search />
+        <div className="container text-center">
+            <div className="row">
+                <div className="col-xl-9 mx-auto">
+                    <h1 className="mb-5">WEATHER APP</h1>
+                    <h4 className="mb-5">SEARCH FOR CURRENT WEATHER DATA</h4>
+                </div>
+                <Search />
+            </div>
         </div>
     );
 };
